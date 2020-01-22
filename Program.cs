71aa270ad77fe6ee7ca3d825bd2101510229ed41
crystalritchey <voltia@yahoo.com>
2020-01-21 19:35:21 -0600

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Assignment1 {
+    public class Person implements IComparable
+    {
+        readonly uint id;
+        string lastName;
+        string firstName;
+        string occupation;
+        readonly DateTime birthday;
+        List<int> residenceIds;
+
+        public Person()
+        {
+            id = 0;
+            lastName = "";
+            firstName = "";
+            occupation = "";
+            birthday = DateTime.Now;
+        }
+
+    public interface CompareTo(object alpha) {
+        }
+    }
